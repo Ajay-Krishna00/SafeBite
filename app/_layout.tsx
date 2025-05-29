233ec7e3d616962a72d70de85c792a67869d29d8
Abhay Murali <abhaymuralim9@gmail.com>
2025-05-29 21:42:17 +0530

    login, signin and onboarding added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,5 +2,9 @@ import "./global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="index" options={{headerShown:false}} />
+    <Stack.Screen name="signup" options={{headerShown:false}} />
+    <Stack.Screen name="onboarding" options={{headerShown:false}} />
+  </Stack>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,64 @@
-import { Text, View } from "react-native";
+import { Link, useRouter } from "expo-router";
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+
+export const SText = ({ className = "", ...props }) => {
+  return <Text className={`text-[#0D0D0D] ${className}`} {...props} />;
+};
+
+export const InputWithLabel = ({
+  name,
+  value,
+  change,
+}: {
+  name: string;
+  value: string;
+  change: (e: string) => void;
+}) => {
+  return (
+    <View>
+      <SText className="text-xl">{name}</SText>
+      <TextInput
+        placeholder={"Your " + name.toLowerCase()}
+        className="bg-gray-100 rounded-sm px-3 h-10"
+        value={value}
+        onChangeText={(e) => {
+          change(e);
+        }}
+      />
+    </View>
+  );
+};
 
 export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  // useEffect(() => {
+  //   const timeout = setTimeout(() => {
+  //     router.push("/onboarding");
+  //   }, 10);
+  // }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-xl text-blue-500 font-bold">Welcome!</Text>
+    <View className="flex-1 justify-center items-center bg-gray-200">
+      <View className="bg-white p-8 gap-8 w-[80%]">
+        <SText className="text-5xl font-semibold m-6 text-center">Login</SText>
+        <InputWithLabel name="Email" value={email} change={setEmail} />
+        <InputWithLabel name="Password" value={password} change={setPassword} />
+        <TouchableOpacity
+          onPress={() => {}}
+          className="bg-blue-400 text-center h-8 justify-center"
+        >
+          Log in
+        </TouchableOpacity>
+        <SText>
+          Don't have an account?Create a new one:{" "}
+          <Link href={"/signup"} className="text-blue-500">
+            Sign Up
+          </Link>
+        </SText>
+      </View>
     </View>
   );
 }
