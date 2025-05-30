53e48ab808aa18b8a49fdaaabf4056dea95e7870
Ajay-Krishna00 <enthusiastajay00@gmail.com>
2025-05-31 03:12:37 +0530

    final

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -4,7 +4,13 @@ import React, { useEffect } from "react";
 import qr from "@/assets/images/qr.jpg";
 import ScanScreen from "../scan";
 import { useRouter } from "expo-router";
-import Animated, { useSharedValue, useAnimatedStyle, withSpring, withRepeat, withTiming } from 'react-native-reanimated';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
 
 const index = () => {
   const router = useRouter();
@@ -13,35 +19,35 @@ const index = () => {
     return {
       transform: [{ scale: offset.value }],
     };
-  })
+  });
   useEffect(() => {
     offset.value = withRepeat(
       withTiming(1.2, { duration: 1000 }), // scale to 2 over 1s
       -1, // repeat forever
-      true // reverse on every cycle
+      true, // reverse on every cycle
     );
   }, []);
   return (
     <>
-    <LinearGradient
-      colors={["#004d00", "#00cc00", "#0000"]}
-      start={{ x: 0.5, y: 0 }}
-      end={{ x: 0.5, y: 1 }}
-      style={{
-        height: "56%",
-        width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <TouchableOpacity
-        className="w-[280px] h-[230px]"
-        onPress={() => router.push("/scan")}
+      <LinearGradient
+        colors={["#004d00", "#00cc00", "#0000"]}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 1 }}
+        style={{
+          height: "56%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <Image
-          source={qr}
+        <TouchableOpacity
           className="w-[280px] h-[230px]"
-          resizeMode="contain"
+          onPress={() => router.push("/scan")}
+        >
+          <Image
+            source={qr}
+            className="w-[280px] h-[230px]"
+            resizeMode="contain"
           />
           <Animated.View
             style={[
@@ -51,19 +57,22 @@ const index = () => {
                 // left: 0,
                 // right: 0,
                 // bottom: 0,
-                justifyContent: 'center',
-                alignItems: 'center',
+                justifyContent: "center",
+                alignItems: "center",
               },
               animatedStyle,
             ]}
           >
             <View className="bg-yellow-300 rounded-full px-6 py-2">
-            <Text className="text-gray-600" style={{ fontSize: 22, fontWeight: 'bold' }}>
-              Tap to scan
-            </Text>
+              <Text
+                className="text-gray-600"
+                style={{ fontSize: 22, fontWeight: "bold" }}
+              >
+                Tap to scan
+              </Text>
             </View>
           </Animated.View>
-      </TouchableOpacity>
+        </TouchableOpacity>
       </LinearGradient>
       <View className="flex-1 items-center justify-center">
         <Text className="text-3xl font-bold text-gray-800 mb-4">
@@ -76,11 +85,12 @@ const index = () => {
           className="mt-6 px-6 py-3 bg-green-500 rounded-full"
           onPress={() => router.push("/scan")}
         >
-          <Text className="text-white text-lg font-semibold">Start Scanning</Text>
+          <Text className="text-white text-lg font-semibold">
+            Start Scanning
+          </Text>
         </TouchableOpacity>
-
       </View>
-      </>
+    </>
   );
 };
 
--- app/services/summary.ts
+++ app/services/summary.ts
@@ -1,7 +1,15 @@
 import { ProductData } from "../product/[id]";
+import { supabase } from "@/lib/supabase";
 
 const handleSummary = async (productData: ProductData) => {
   try {
+    const { data: user } = await supabase.auth.getUser();
+    const { data } = await supabase
+      .from("Customerdetails")
+      .select()
+      .eq("id", user?.user?.id);
+    const allergies = data?.[0].allergies;
+    const conditions = data?.[0].medical_conditions;
     const res = await fetch("https://safebite-28tg.onrender.com/api/summary", {
       method: "POST",
       headers: {
@@ -9,8 +17,8 @@ const handleSummary = async (productData: ProductData) => {
       },
       body: JSON.stringify({
         userProfile: {
-          allergies: ["dairy", "soy"],
-          conditions: ["hypertension", "diabetes"],
+          allergies: allergies,
+          conditions: conditions,
         },
         productData: productData,
       }),
