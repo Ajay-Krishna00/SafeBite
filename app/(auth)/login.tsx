f75a9d8b74d40cf928bf49056c837051231e93d9
Abhay Murali <abhaymuralim9@gmail.com>
2025-05-30 15:16:05 +0530

    minute changes

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -33,21 +33,24 @@ export const InputWithLabel = ({
 function login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const router = useRouter();
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     router.push("/onboarding");
-  //   }, 10);
-  // }, []);
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
 
+  const router = useRouter();
+
+  async function handleLogin() {
+    
+  }
+    
   return (
     <View className="flex-1 justify-center items-center bg-gray-200">
       <View className="bg-white p-8 gap-8 w-[80%] rounded-lg shadow-lg">
         <SText className="text-5xl font-semibold m-6 text-center">Login</SText>
         <InputWithLabel name="Email" value={email} change={setEmail} />
         <InputWithLabel name="Password" value={password} change={setPassword} />
+        {error && <SText className="text-red-600">{error}</SText>}
         <TouchableOpacity
-          onPress={() => {}}
+          onPress={handleLogin}
           className="bg-green-700 text-center h-8 justify-center p-1 rounded-md"
         >
           <Text className="text-white text-lg font-bold text-center">
--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -1,4 +1,4 @@
-import { Link, router } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import React, { useState } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { InputWithLabel, SText } from "./login";
@@ -7,6 +7,12 @@ const signup = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+  async function handleSignup() {}
+
   return (
     <View className="flex-1 justify-center items-center bg-gray-200">
       <View className="bg-white p-8 gap-8 w-[80%] rounded-lg shadow-lg">
@@ -16,6 +22,7 @@ const signup = () => {
         <InputWithLabel name="Username" value={username} change={setUsername} />
         <InputWithLabel name="Email" value={email} change={setPassword} />
         <InputWithLabel name="Password" value={password} change={setPassword} />
+        {error && <SText className="text-red-600">{error}</SText>}
         <TouchableOpacity
           onPress={() => router.push("/onboarding")}
           className="bg-green-700 text-center h-8 justify-center p-1 rounded-md"
